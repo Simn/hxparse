25e939b36db87630fa6fa81eb607eafa37955fa0
Simon Krajewski <simon@haxe.org>
2013-09-23 10:03:14 +0200

    move UnexpectedChar to its own file

--- src/hxparse/Lexer.hx
+++ src/hxparse/Lexer.hx
@@ -1,38 +1,6 @@
 package hxparse;
 
 /**
-	UnexpectedChar is thrown by `Lexer.token` if it encounters a character for
-	which no state transition is defined.
-**/
-class UnexpectedChar {
-	
-	/**
-		The character which caused `this` exception.
-	**/
-	public var char:String;
-	
-	/**
-		The position in the input where `this` exception occured.
-	**/
-	public var pos:Position;
-	
-	/**
-		Creates a new instance of UnexpectedChar.
-	**/
-	public function new(char, pos) {
-		this.char = char;
-		this.pos = pos;
-	}
-	
-	/**
-		Returns a readable representation of `this` exception.
-	**/
-	public function toString() {
-		return '$pos: Unexpected $char';
-	}
-}
-
-/**
 	Lexer matches a sequence of characters against a set of rule patterns.
 	
 	An instance of Lexer is created once for each input and maintains state
