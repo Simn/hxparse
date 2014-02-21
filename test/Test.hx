800985d763ed637290159d61a47d52ef68bfa1ae
Simon Krajewski <simon@haxe.org>
2014-02-21 10:07:49 +0100

    remove haxe parser (moved to https://github.com/Simn/haxeparser)

--- test/Test.hx
+++ test/Test.hx
@@ -1,28 +1,5 @@
-import haxe.Resource;
-import hxparse.NoMatch;
-import hxparse.Unexpected;
-
 class Test {
-	
 	static function main() {
-		function run() {
-			var i = byte.ByteData.ofString(Resource.getString('HaxeFile'));
-			var parser = new HaxeParser(i, 'HaxeFile.hx');
-			parser.define("js");
-			parser.define("foo", 1.3);
-			var data = try {
-				parser.parse();
-			} catch(e:NoMatch<Dynamic>) {
-				throw e.pos.format(i) + ": Unexpected " +e.token.tok;
-			} catch(e:Unexpected<Dynamic>) {
-				throw e.pos.format(i) + ": Unexpected " + e.token.tok;
-			}
-			return data;
-		}
-		var r = haxe.Timer.measure(run);
-		trace(r.decls);
-		//trace(r.decls);
-		
 		var parser = new PrintfParser(byte.ByteData.ofString("Valu$$e: $-050.2f kg"));
 		trace(parser.parse());
 		
