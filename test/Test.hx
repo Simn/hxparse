5327ff8df2ab5a16517e9186b5400a93b5b2da7d
Simon Krajewski <simon@haxe.org>
2014-04-02 22:22:37 +0200

    add ArithmeticParser example

--- test/Test.hx
+++ test/Test.hx
@@ -2,10 +2,10 @@ class Test {
 	static function main() {
 		var parser = new PrintfParser(byte.ByteData.ofString("Valu$$e: $-050.2f kg"));
 		trace(parser.parse());
-		
+
 		var parser = new JSONParser(byte.ByteData.ofString('{ "key": [true, false, null], "other\tkey": [12, 12.1, 0, 0.1, 0.9e, 0.9E, 9E-] }'), "jsontest");
 		trace(parser.parse());
-		
+
 		// Using haxe.Utf8
 		var value = 'âêùあ𠀀';
 		var lexer = new UnicodeTestLexer( byte.ByteData.ofString( value ), 'uft8-test' );
@@ -17,5 +17,23 @@ class Test {
 			trace(_e);
 		}
 		trace( tokens );
+
+		var numTests = 0;
+		function eq(f1:Float, s:String) {
+			++numTests;
+			var parser = new ArithmeticParser(new ArithmeticParser.ArithmeticLexer(byte.ByteData.ofString(s)), ArithmeticParser.ArithmeticLexer.tok);
+			var f2 = ArithmeticParser.ArithmeticEvaluator.eval(parser.parse());
+			if (f1 != f2) {
+				trace('$f2 should be $f2');
+			}
+		}
+		eq(1, "1");
+		eq(2, "1 + 1");
+		eq(6, "2 * 3");
+		eq(2, "6 / 3");
+		eq(1.5, "3 / 2");
+		eq(10, "2 * 3 + 4");
+		eq(14, "2 * (3 + 4)");
+		trace('Done $numTests tests');
 	}
 }
