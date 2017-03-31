1343cc906a0bc83d81de1d2d30d0a7c360afea57
Simon Krajewski <simon@haxe.org>
2017-03-31 21:10:26 +0200

    fix tests

--- test/Test.hx
+++ test/Test.hx
@@ -6,7 +6,7 @@ class Test {
 		var parser = new PrintfParser(byte.ByteData.ofString("Valu$$e: $-050.2f kg"));
 		trace(parser.parse());
 
-		var parser = new JSONParser(byte.ByteData.ofString('{ "key": [true, false, null], "other\tkey": [12, 12.1, 0, 0.1, 0.9e, 0.9E, 9E-] }'), "jsontest");
+		var parser = new JSONParser(byte.ByteData.ofString('{ "key": [true, false, null], "other\tkey": [12, 12.1, 0, 0.1, 0.9e1, 0.9E1, 9E-1] }'), "jsontest");
 		trace(parser.parseJson());
 
 		// Using haxe.Utf8
